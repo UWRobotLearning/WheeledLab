c2999d840375e96d1004772a5f8c3e4f2cc7b52b
kohonda <0905honda@gmail.com>
2025-06-12 18:22:06 +0900

    Add local wheeledlab-suite to dependencies and make the structure flattened

--- source/wheeledlab_tasks/__init__.py
+++ source/wheeledlab_tasks/__init__.py
@@ -13,36 +13,36 @@ import wheeledlab_tasks.elevation.config.agents.mushr as mushr_elevation_agents
 
 gym.register(
     id="Isaac-MushrDriftRL-v0",
-    entry_point='isaaclab.envs:ManagerBasedRLEnv',
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point":MushrDriftRLEnvCfg,
+        "env_cfg_entry_point": MushrDriftRLEnvCfg,
         "rsl_rl_cfg_entry_point": f"{mushr_drift_agents.__name__}.rsl_rl_ppo_cfg:MushrPPORunnerCfg",
-        "play_env_cfg_entry_point": MushrDriftPlayEnvCfg
-    }
+        "play_env_cfg_entry_point": MushrDriftPlayEnvCfg,
+    },
 )
 
 
 gym.register(
     id="Isaac-MushrVisualRL-v0",
-    entry_point='isaaclab.envs:ManagerBasedRLEnv',
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point":MushrVisualRLEnvCfg,
+        "env_cfg_entry_point": MushrVisualRLEnvCfg,
         "rsl_rl_cfg_entry_point": f"{mushr_visual_agents.__name__}.rsl_rl_ppo_cfg:MushrPPORunnerCfg",
-        "play_env_cfg_entry_point": MushrVisualPlayEnvCfg
-    }
+        "play_env_cfg_entry_point": MushrVisualPlayEnvCfg,
+    },
 )
 
 gym.register(
     id="Isaac-MushrElevationRL-v0",
-    entry_point='isaaclab.envs:ManagerBasedRLEnv',
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point": MushrElevationRLEnvCfg,
         "rsl_rl_cfg_entry_point": f"{mushr_elevation_agents.__name__}.rsl_rl_ppo_cfg:MushrPPORunnerCfg",
-        "play_env_cfg_entry_point": MushrElevationPlayEnvCfg
-    }
+        "play_env_cfg_entry_point": MushrElevationPlayEnvCfg,
+    },
 )
 
 #######################################
@@ -54,10 +54,10 @@ import wheeledlab_tasks.drifting.config.agents.f1tenth as f1tenth_drift_agents
 
 gym.register(
     id="Isaac-F1TenthDriftRL-v0",
-    entry_point='isaaclab.envs:ManagerBasedRLEnv',
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point":F1TenthDriftRLEnvCfg,
+        "env_cfg_entry_point": F1TenthDriftRLEnvCfg,
         "rsl_rl_cfg_entry_point": f"{f1tenth_drift_agents.__name__}.rsl_rl_ppo_cfg:F1TenthPPORunnerCfg",
-    }
+    },
 )
